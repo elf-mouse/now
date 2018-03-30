727964943b7bd27e9fa524ac490c4e79bbf0e83b
elf-mouse <ifmouz@gmail.com>
2018-03-30 15:24:52 +0800

    update

--- pwa/sw.js
+++ pwa/sw.js
@@ -12,12 +12,56 @@ workbox.precaching.precacheAndRoute([
     "revision": "2d5a3f574504ecfa9311ebffb34d3e29"
   },
   {
+    "url": "favicon.ico",
+    "revision": "29e32bd79c18993464e8600b7e8fa5a4"
+  },
+  {
+    "url": "img/icons/icon-128x128.png",
+    "revision": "f09c7e1b7e6b67217fbdbdeefa502ef4"
+  },
+  {
+    "url": "img/icons/icon-144x144.png",
+    "revision": "d78676253e313534aa21661c5c5e29e5"
+  },
+  {
+    "url": "img/icons/icon-152x152.png",
+    "revision": "94cadb1dc1a9d05e7dc0a64cbfe14e0b"
+  },
+  {
+    "url": "img/icons/icon-192x192.png",
+    "revision": "3a8764970e2d9f1c13340218c53c26b9"
+  },
+  {
+    "url": "img/icons/icon-384x384.png",
+    "revision": "c8495699e3b8d02b89b271e30bd85f0e"
+  },
+  {
+    "url": "img/icons/icon-512x512.png",
+    "revision": "cbdbb679139048e03ac977758a5c9f20"
+  },
+  {
+    "url": "img/icons/icon-72x72.png",
+    "revision": "b75809917482a05197e179f1d82dd4d5"
+  },
+  {
+    "url": "img/icons/icon-96x96.png",
+    "revision": "f4df1a04b681b720a77d90749524d686"
+  },
+  {
+    "url": "img/logo.svg",
+    "revision": "bae9298c45608e2a87e027ce96827f44"
+  },
+  {
     "url": "index.html",
     "revision": "9ff64ab68f6adaaadcc1673beccdc10e"
   },
   {
     "url": "js/main.js",
-    "revision": "091aa8ec24e9a855101693d5576cf89c"
+    "revision": "72377ed7ab52f9f22aa94702b4e9fadd"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "4e8d33f87bdf61b06b96cdb353f8bc8e"
   },
   {
     "url": "workbox-sw.js",
