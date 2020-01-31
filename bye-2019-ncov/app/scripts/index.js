da4844a70f1b5ef5f61f629baa12501fe4544591
Elf-mousE <ifmouz@gmail.com>
2020-01-31 22:02:21 +0800

    update

--- bye-2019-ncov/app/scripts/index.js
+++ bye-2019-ncov/app/scripts/index.js
@@ -17,6 +17,7 @@ const questionsCount = questions.length;
 
 //答题流程 var
 var travel = {
+  rightAnswers: 0, // 新增
   grade: 0,
   tu: '',
   des: '防疫知识问答，请收藏转发给家人朋友',
@@ -74,6 +75,7 @@ var travel = {
     for (var j = 0; j < travel.test[i].item.length; j++) {
       $('.ans')
         .eq(j)
+        .off('click')
         .show();
       $('.ans1')
         .eq(j)
@@ -86,10 +88,8 @@ var travel = {
       function message(t) {
         $('.ans')
           .eq(j)
-          .off('tap');
-        $('.ans')
-          .eq(j)
-          .on('tap', function() {
+          .on('click', function() {
+            // console.log('ans', $(this), travel.rightAnswers);
             //对弹出面板设置
             $(this).css({ 'background-color': '#7ee9ff' });
             $(this)
@@ -108,7 +108,9 @@ var travel = {
               .css({ color: '#002d69' });
             if (t == 1) {
               $('.sh_an').html('正确！');
-              travel.grade = travel.grade + 10;
+              // console.log('gg');
+              travel.rightAnswers += 1;
+              // travel.grade = travel.grade + 10;
               // console.log(travel.grade);
             } else {
               $('.sh_an').html('答错了！');
@@ -132,8 +134,9 @@ var travel = {
               $('.shelter').show(300);
               $('.shelter').addClass('fadeIn');
               $('.sh_back').addClass('rotateIn');
-              $('.tip_btn').off('tap');
-              $('.tip_btn').on('tap', function() {
+              $('.tip_btn').off('click');
+              $('.tip_btn').on('click', function() {
+                // console.log('tip_btn', $(this), travel.rightAnswers);
                 // $(this).css({ 'background-color': '#118b40' });
                 // $(this)
                 //   .children()
@@ -156,8 +159,10 @@ var travel = {
                   $('.sh_back').removeClass('rotateIn');
                   $('.sh_back').removeClass('rotateOut');
                 }, 700);
-                if (i !== questionsCount - 1) travel.go(i + 1);
-                else {
+
+                if (i !== questionsCount - 1) {
+                  travel.go(i + 1);
+                } else {
                   $('.content').hide();
                   $('.result').show();
                   travel.conclude();
@@ -169,19 +174,21 @@ var travel = {
     }
   },
   conclude: function() {
-    var grade = travel.grade;
-    var g = grade / 10;
-    var gindex;
-    if (g > 11) {
-      gindex = 5;
-    } else {
-      gindex = Math.ceil(g / 3);
-      // console.log(g + '----' + gindex);
-    }
-    travel.des = travel.result[gindex].com;
+    // var grade = travel.grade;
+    // var g = grade / 10;
+    // var gindex;
+    // if (g > 11) {
+    //   gindex = 5;
+    // } else {
+    //   gindex = Math.ceil(g / 3);
+    //   // console.log(g + '----' + gindex);
+    // }
+    // travel.des = travel.result[gindex].com;
     setTimeout(function() {
       // $('.re_p').html('答对' + g + '题!<br/>' + travel.result[gindex].asses);
-      $('.re_p').html(`<b>答题完毕，答对${g}题。</b><br/>科学防护，从我做起！`);
+      $('.re_p').html(
+        `<b>答题完毕，答对${travel.rightAnswers}题。</b><br/>科学防护，从我做起！`
+      );
 
       $('.re_p').addClass('large');
     }, 400);
@@ -224,31 +231,31 @@ $('#re_play').on('tap', function() {
     .css({ color: '#036eb8' });
   window.location.reload();
 });
-$('#share').on('tap', function() {
-  $(this).css({ 'background-color': '#7ee9ff' });
-  $(this)
-    .children()
-    .eq(0)
-    .children()
-    .attr({ src: '%host%/news/2015/travel/earth5.png' });
-  $(this)
-    .children()
-    .eq(1)
-    .children()
-    .css({ color: '#036eb8' });
-  mShare.init();
-});
+// $('#share').on('tap', function() {
+//   $(this).css({ 'background-color': '#7ee9ff' });
+//   $(this)
+//     .children()
+//     .eq(0)
+//     .children()
+//     .attr({ src: '%host%/news/2015/travel/earth5.png' });
+//   $(this)
+//     .children()
+//     .eq(1)
+//     .children()
+//     .css({ color: '#036eb8' });
+//   mShare.init();
+// });
 //share面板
-$('.share').on('tap', function() {
-  $(this).hide();
-  $('#share')
-    .children()
-    .eq(0)
-    .children()
-    .attr({ src: '%host%/news/2015/travel/earth4.png' });
-  $('#share')
-    .children()
-    .eq(1)
-    .children()
-    .css({ color: '#7ee9ff' });
-});
+// $('.share').on('tap', function() {
+//   $(this).hide();
+//   $('#share')
+//     .children()
+//     .eq(0)
+//     .children()
+//     .attr({ src: '%host%/news/2015/travel/earth4.png' });
+//   $('#share')
+//     .children()
+//     .eq(1)
+//     .children()
+//     .css({ color: '#7ee9ff' });
+// });
