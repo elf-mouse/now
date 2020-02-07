748ad9eb46fad5ddd2790030822416581c7f9b1c
Elf-mousE <ifmouz@gmail.com>
2020-02-07 19:16:19 +0800

    update

--- 2020-school-open/app/scripts/index.js
+++ 2020-school-open/app/scripts/index.js
@@ -32,6 +32,6 @@ function ajax(url, success) {
 }
 
 let url = '/data/wuhan2020.json'; // local
-// let url = '/now/sh-2019-ncov/dist/data/wuhan2020.json'; // test
+// let url = '/now/2020-school-open/dist/data/wuhan2020.json'; // test
 // let url = '/schoolopens/2020index/data/wuhan2020.json'; // yiban
 ajax(url, wuhan2020);
